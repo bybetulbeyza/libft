befa192451a4a68f7bc4591319dfe70316a670b0
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-08-25 14:01:36 +0000

    add ft_strdup.c, ft_strjoin.c, ft_substr.c

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/25 10:11:52 by betdemir          #+#    #+#             */
+/*   Updated: 2026/08/25 12:48:15 by betdemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	size;
+	char	*arr;
+
+	if (!s)
+		return (NULL);
+	size = ft_strlen(s);
+	if (start > size)
+		return (ft_strdup(""));
+	if (len > size - start)
+		len = size - start;
+	arr = malloc(len + 1);
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		arr[i] = s[start + i];
+		i++;
+	}
+	arr[i] = '\0';
+	return (arr);
+}
