f667c1e101e4de076117baedbf4aac9ee0cb41ed
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-08-28 08:58:32 +0000

    add ft_lstsize.c

---
+++ ft_lstsize.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/28 08:40:30 by betdemir          #+#    #+#             */
+/*   Updated: 2026/08/28 08:58:01 by betdemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_lstsize(t_list *lst)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (lst)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
