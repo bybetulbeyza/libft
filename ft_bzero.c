08f2a28ac9924ba67fd748f0402bc40b95765065
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-09-01 14:18:26 +0000

    renamed bzero to ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 12:39:06 by betdemir          #+#    #+#             */
-/*   Updated: 2026/08/22 16:36:59 by betdemir         ###   ########.fr       */
+/*   Updated: 2026/09/01 12:16:14 by betdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*arr;
 	size_t			i;
