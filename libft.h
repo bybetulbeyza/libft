bb6ec4548cc7bb45700769d1b87d7b944c950643
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-09-02 08:33:29 +0000

    modified libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 17:50:35 by betdemir          #+#    #+#             */
-/*   Updated: 2026/09/01 14:10:44 by betdemir         ###   ########.fr       */
+/*   Updated: 2026/09/02 08:32:43 by betdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlcat(char *dst, const char *src, size_t size);
-int				toupper(int c);
+int				ft_toupper(int c);
 int				ft_tolower(int c);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
