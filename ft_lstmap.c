24254f6d3a69426b6734b54aebc5591a1bd42f0b
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-09-02 12:24:05 +0000

    edited ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 14:56:21 by betdemir          #+#    #+#             */
-/*   Updated: 2026/09/01 13:06:54 by betdemir         ###   ########.fr       */
+/*   Updated: 2026/09/02 12:14:55 by betdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new;
+	t_list	*new_node;
+	void	*content;
 
-	new_list = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
-	while (lst)
+	new_list = NULL;
+	while (lst != NULL)
 	{
-		new = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		new_node = ft_lstnew(content);
+		if (new_node == NULL)
+		{
+			del(content);
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
-		ft_lstadd_back(&new_list, new);
 	}
 	return (new_list);
 }
