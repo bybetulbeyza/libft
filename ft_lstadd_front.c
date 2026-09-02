4abdddb3c665531fbe233f1726e61fae4f27af64
Betül Beyza Demirel <demirelbetulbeyza@gmail.com>
2026-09-02 12:23:32 +0000

    edited ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/27 19:46:26 by betdemir          #+#    #+#             */
-/*   Updated: 2026/08/27 20:41:03 by betdemir         ###   ########.fr       */
+/*   Updated: 2026/09/02 12:16:50 by betdemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (lst == NULL || new == NULL)
+		return ;
 	new->next = *lst;
 	*lst = new;
 }
